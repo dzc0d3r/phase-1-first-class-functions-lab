b620ed026441118b03963977c9e3ec6da43f1c09
dzc0d3r <m4dcod3r@protonmail.com>
2023-07-23 03:23:40 +0100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+
+
+const returnFirstTwoDrivers = function (drivers) {
+    const cloned = [...drivers]
+    return cloned.slice(0, 2)
+}
+
+
+const returnLastTwoDrivers = (drivers) => {
+    const cloned = [...drivers]
+    return cloned.slice(-2)
+}
+
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+
+
+function createFareMultiplier(multiplier) {
+    const fareMultiplier = function (multiplier) {
+        return multiplier * multiplier
+    }
+    return fareMultiplier
+}
+function fareDoubler(doubler) {
+    return doubler * 2
+}
+
+const fareTripler = (tripler) => { return tripler * 3 }
+
+
+
+function selectDifferentDrivers(drivers, callback) {
+    return callback(drivers)
+}
